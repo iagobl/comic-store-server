a0da9ab1b74bd7bcd3090fdbb3104f2251fddee6
Iago <iagoblopez@gmail.com>
2022-05-19 14:06:40 +0200

    CollectionController class implemented

---
+++ src/main/java/com/iagobl/server/controller/CollectionController.java
@@ -0,0 +1,59 @@
+package com.iagobl.server.controller;
+
+import com.iagobl.server.model.Collection;
+import com.iagobl.server.services.CollectionServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api-spring/collection")
+public class CollectionController {
+
+    @Autowired
+    public CollectionServices collectionServices;
+
+    @GetMapping
+    public List<Collection> findAll(){
+        return collectionServices.findAll();
+    }
+
+    @GetMapping("/findByName/{name}")
+    public Collection findName(@PathVariable(value = "name") String name){
+        Optional<Collection> collection = collectionServices.findByName(name);
+        if(!collection.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Collection not found"));
+        }
+
+        return collection.get();
+    }
+
+    @PostMapping
+    public Collection save(@RequestBody Collection collection){
+        return collectionServices.collectionSave(collection);
+    }
+
+    @PutMapping("/{id}")
+    public Collection update(@PathVariable(value = "id") Long id,
+                             @RequestParam(required = true, value = "name") String name,
+                             @RequestParam(required = true, value = "editorial") String editorial){
+        return collectionServices.collectionUpdate(id, name, editorial);
+    }
+
+
+    public void delete(@PathVariable(value = "id") Long id){
+
+        Optional<Collection> collectionDelete = collectionServices.findById(id);
+        if(!collectionDelete.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Collection not found"));
+        }
+
+        collectionServices.collectionDelete(id);
+    }
+
+
+}
