ceccd33875dbe2a388d177f97913aaaffb3863d8
Iago <iagoblopez@gmail.com>
2022-05-19 13:57:30 +0200

    Modification of the CollectionRepository file

--- src/main/java/com/iagobl/server/repository/CollectionRepository.java
+++ src/main/java/com/iagobl/server/repository/CollectionRepository.java
@@ -2,9 +2,15 @@ package com.iagobl.server.repository;
 
 import com.iagobl.server.model.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
 
+    @Query("SELECT a FROM Collection a  where a.name = :name")
+    Optional<Collection> findByName(@Param("name") String name);
 }
